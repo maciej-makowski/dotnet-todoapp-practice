06d060dd2714d1aeca3937d66615e3a288163e22
rlyyah <39671388+rlyyah@users.noreply.github.com>
2020-07-26 20:36:45 +0200

    Fixed formatting

--- TodoApp.Cli/Commands/AddCommand.cs
+++ TodoApp.Cli/Commands/AddCommand.cs
@@ -5,13 +5,10 @@ namespace TodoApp.Cli.Commands
 {
     [Verb("add", HelpText = "Adds new todo item")]
     public class AddCommand
-    {
-        /*
+    {/*
         [Option('d', "destination", HelpText = "Path to source file to load", Default = "./data/todo.json")]
         public string Source { get; set; }
-        */
-        
-        /*public async Task Run()
+        public async Task Run()
         {
             // TODO ...
             var loader = new TodoJsonFileLoader();
