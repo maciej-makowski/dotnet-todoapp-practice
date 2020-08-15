9f64f2459b3e368074ec161409325b89ca293c32
rlyyah <39671388+rlyyah@users.noreply.github.com>
2020-08-15 15:09:32 +0200

    Delete Commented

--- TodoApp.Cli/Repository/TodoRepository.cs
+++ TodoApp.Cli/Repository/TodoRepository.cs
@@ -10,7 +10,7 @@ namespace TodoApp.Cli.Repository
     {
         private static int NEXT_ID = 0;
 
-        public IList<ITodo> Tasks { get; set; } = new List<ITodo>();
+        private IList<ITodo> Tasks { get; set; } = new List<ITodo>();
 
         public async Task LoadItems(string path)
         {
